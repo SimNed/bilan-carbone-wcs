3dee7a1395ad3219d61b4aa5490b46d9d401c05d
Simon Nedjari <simon.nedjari@outlook.com>
2024-01-30 15:26:58 +0100

    add: head infos

--- web-app/src/components/Layout/Layout.tsx
+++ web-app/src/components/Layout/Layout.tsx
@@ -8,13 +8,12 @@ export default function Layout({ children }: { children: ReactNode }) {
   return (
     <>
       <Head>
-        <title>The Good Corner</title>
+        <title>Bilan Carbone</title>
         <meta
           name="description"
-          content="The Good Corner, pour acheter et vendre vos articles neufs et d'occasion."
+          content="Mesurer son impact carbone"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <MainContent>{children}</MainContent>
