9032feac451cd4ace39958d5601e2ba2b6faf19b
Simon Nedjari <simon.nedjari@outlook.com>
2024-01-30 15:32:53 +0100

    clean: AdResolver, remove TagResolver

--- back-end/src/resolvers/AdResolver.ts
+++ back-end/src/resolvers/AdResolver.ts
@@ -15,31 +15,31 @@ import User from "../entities/user";
 
 @Resolver()
 export class AdResolver {
-  @Query(() => [Ad])
-  ads(@Arg("category", { nullable: true }) category: number) {
-    return Ad.getAds(category ?? undefined);
-  }
+  // @Query(() => [Ad])
+  // ads(@Arg("category", { nullable: true }) category: number) {
+  //   return Ad.getAds(category ?? undefined);
+  // }
 
-  @Query(() => Ad)
-  ad(@Arg("id", () => ID) id: string) {
-    return Ad.getAdById(id);
-  }
+  // @Query(() => Ad)
+  // ad(@Arg("id", () => ID) id: string) {
+  //   return Ad.getAdById(id);
+  // }
 
-  @Authorized()
-  @Mutation(() => Ad)
-  createAd(@Args() args: CreateOrUpdateAd, @Ctx() { user }: Context) {
-    return Ad.saveNewAd({ ...args, owner: user as User });
-  }
+  // @Authorized()
+  // @Mutation(() => Ad)
+  // createAd(@Args() args: CreateOrUpdateAd, @Ctx() { user }: Context) {
+  //   return Ad.saveNewAd({ ...args, owner: user as User });
+  // }
 
-  @Authorized()
-  @Mutation(() => Ad)
-  updateAd(@Arg("id", () => ID) id: string, @Args() args: CreateOrUpdateAd) {
-    return Ad.updateAd(id, args);
-  }
+  // @Authorized()
+  // @Mutation(() => Ad)
+  // updateAd(@Arg("id", () => ID) id: string, @Args() args: CreateOrUpdateAd) {
+  //   return Ad.updateAd(id, args);
+  // }
 
-  @Authorized()
-  @Mutation(() => Ad)
-  async deleteAd(@Arg("id", () => ID) id: string) {
-    return Ad.deleteAd(id);
-  }
+  // @Authorized()
+  // @Mutation(() => Ad)
+  // async deleteAd(@Arg("id", () => ID) id: string) {
+  //   return Ad.deleteAd(id);
+  // }
 }
