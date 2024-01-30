e9fe5d068e62df6ec8b3c31965c5135339e04a92
Simon Nedjari <simon.nedjari@outlook.com>
2024-01-30 15:32:03 +0100

    clean: ad

--- back-end/src/entities/ad.ts
+++ back-end/src/entities/ad.ts
@@ -10,8 +10,6 @@ import {
 } from "typeorm";
 import { ObjectType, Field, ID, Float } from "type-graphql";
 
-import Category from "./category";
-import Tag from "./tag";
 import { CreateOrUpdateAd } from "./ad.args";
 import User from "./user";
 
@@ -54,14 +52,14 @@ class Ad extends BaseEntity {
   @Field()
   createdAt!: Date;
 
-  @ManyToOne(() => Category, (category) => category.ads, { eager: true })
-  @Field(() => Category)
-  category!: Category;
+  // @ManyToOne(() => Category, (category) => category.ads, { eager: true })
+  // @Field(() => Category)
+  // category!: Category;
 
-  @JoinTable({ name: "TagsForAds" })
-  @ManyToMany(() => Tag, (tag) => tag.ads, { eager: true })
-  @Field(() => [Tag])
-  tags!: Tag[];
+  // @JoinTable({ name: "TagsForAds" })
+  // @ManyToMany(() => Tag, (tag) => tag.ads, { eager: true })
+  // @Field(() => [Tag])
+  // tags!: Tag[];
 
   constructor(ad?: AdArgs) {
     super();
@@ -76,60 +74,60 @@ class Ad extends BaseEntity {
     }
   }
 
-  static async saveNewAd(adData: AdArgs): Promise<Ad> {
-    const newAd = new Ad(adData);
-    if (adData.categoryId) {
-      const category = await Category.getCategoryById(adData.categoryId);
-      newAd.category = category;
-    }
-    if (adData.tagIds) {
-      // Promise.all will call each function in array passed as argument and resolve when all are resolved
-      newAd.tags = await Promise.all(adData.tagIds.map(Tag.getTagById));
-    }
-    const savedAd = await newAd.save();
-    console.log(`New ad saved: ${savedAd.getStringRepresentation()}.`);
-    return savedAd;
-  }
+  // static async saveNewAd(adData: AdArgs): Promise<Ad> {
+  //   const newAd = new Ad(adData);
+  //   if (adData.categoryId) {
+  //     const category = await Category.getCategoryById(adData.categoryId);
+  //     newAd.category = category;
+  //   }
+  //   if (adData.tagIds) {
+  //     // Promise.all will call each function in array passed as argument and resolve when all are resolved
+  //     newAd.tags = await Promise.all(adData.tagIds.map(Tag.getTagById));
+  //   }
+  //   const savedAd = await newAd.save();
+  //   console.log(`New ad saved: ${savedAd.getStringRepresentation()}.`);
+  //   return savedAd;
+  // }
 
-  static async getAds(categoryId?: number): Promise<Ad[]> {
-    const ads = await Ad.find({
-      where: { category: { id: categoryId } },
-      order: { createdAt: "DESC" },
-    });
-    return ads;
-  }
+  // static async getAds(categoryId?: number): Promise<Ad[]> {
+  //   const ads = await Ad.find({
+  //     where: { category: { id: categoryId } },
+  //     order: { createdAt: "DESC" },
+  //   });
+  //   return ads;
+  // }
 
-  static async getAdById(id: string): Promise<Ad> {
-    const ad = await Ad.findOne({
-      where: { id },
-    });
-    if (!ad) {
-      throw new Error(`Ad with ID ${id} does not exist.`);
-    }
-    return ad;
-  }
+  // static async getAdById(id: string): Promise<Ad> {
+  //   const ad = await Ad.findOne({
+  //     where: { id },
+  //   });
+  //   if (!ad) {
+  //     throw new Error(`Ad with ID ${id} does not exist.`);
+  //   }
+  //   return ad;
+  // }
 
-  static async deleteAd(id: string): Promise<Ad> {
-    const ad = await Ad.getAdById(id);
-    await Ad.delete(id);
-    return ad;
-  }
+  // static async deleteAd(id: string): Promise<Ad> {
+  //   const ad = await Ad.getAdById(id);
+  //   await Ad.delete(id);
+  //   return ad;
+  // }
 
-  static async updateAd(id: string, partialAd: CreateOrUpdateAd): Promise<Ad> {
-    const ad = await Ad.getAdById(id);
-    Object.assign(ad, partialAd);
+  // static async updateAd(id: string, partialAd: CreateOrUpdateAd): Promise<Ad> {
+  //   const ad = await Ad.getAdById(id);
+  //   Object.assign(ad, partialAd);
 
-    if (partialAd.categoryId) {
-      ad.category = await Category.getCategoryById(partialAd.categoryId);
-    }
-    if (partialAd.tagIds) {
-      ad.tags = await Promise.all(partialAd.tagIds.map(Tag.getTagById));
-    }
+  //   if (partialAd.categoryId) {
+  //     ad.category = await Category.getCategoryById(partialAd.categoryId);
+  //   }
+  //   if (partialAd.tagIds) {
+  //     ad.tags = await Promise.all(partialAd.tagIds.map(Tag.getTagById));
+  //   }
 
-    await ad.save();
-    ad.reload();
-    return ad;
-  }
+  //   await ad.save();
+  //   ad.reload();
+  //   return ad;
+  // }
 
   getStringRepresentation(): string {
     return `${this.id} | ${this.title} | ${this.owner} | ${this.price} €`;
