2258d4d8597d409148e12b375aab4a5705a46a89
Valentin Coppin <valentincoppin@hotmail.fr>
2024-02-01 16:03:01 +0100

    feat(carbonEmissionPage): create the button + route for carbon emiision page

---
+++ web-app/src/components/Buttons/CarbonEmissionButton/CarbonEmissionButton.styled.tsx
@@ -0,0 +1,7 @@
+import { SECONDARY_COLOR } from '../../../styles/constants';
+import styled from 'styled-components';
+
+export const CarbonEmissionButtonStyled = styled.button`
+  padding: 20px;
+  background-color: ${SECONDARY_COLOR};
+`;
---
+++ web-app/src/styles/constants.ts
@@ -0,0 +1,2 @@
+export const PRIMARY_COLOR = '#77BB55';
+export const SECONDARY_COLOR = '#ADC6A1';
