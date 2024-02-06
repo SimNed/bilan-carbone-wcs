2623cf5359bf3618a5b81df16a7aedc7246d1a59
Aurelie Menin <atypik77@hotmail.fr>
2024-02-06 15:27:46 +0100

    refacto banner component

--- web-app/src/pages/carbonEmissionPage/view/HeaderView/HeaderView.tsx
+++
@@ -1,9 +0,0 @@
-import { PRIMARY_COLOR } from '@/styles/constants';
-import styled from 'styled-components';
-
-export const HeaderView = styled.h1`
-  font-size: 48px;
-  background-color: ${PRIMARY_COLOR};
-  padding: 20px;
-  border-radius: 10px;
-`;
