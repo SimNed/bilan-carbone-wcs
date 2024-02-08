d0c614f4c87c90ee1e92b9352ae3bc9912112295
SimNed <simon.nedjari@outlook.com>
2024-02-08 19:38:16 +0100

    add user entity, args, and sign-in page model from the good corner

--- back-end/src/entities/user.ts
+++ back-end/src/entities/user.ts
@@ -1,45 +1,78 @@
-// import { Field, ID, ObjectType } from "type-graphql";
-// import {
-//   BaseEntity,
-//   Column,
-//   Entity,
-//   OneToMany,
-//   PrimaryGeneratedColumn,
-// } from "typeorm";
-// import { CreateOrUpdateUser, SignInUser } from "./user.args";
-// import Ride from "./ride";
+import { Field, ID, ObjectType } from "type-graphql";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from "typeorm";
+import { compare, hash } from "bcrypt";
+import { CreateOrUpdateUser, SignInUser } from "./user.args";
+import Ride from "./ride";
 
-// @Entity("AppUser")
-// @ObjectType()
-// class User extends BaseEntity {
-//   @PrimaryGeneratedColumn("uuid")
-//   @Field(() => ID)
-//   id!: string;
+@Entity("AppUser")
+@ObjectType()
+class User extends BaseEntity {
+  @PrimaryGeneratedColumn("uuid")
+  @Field(() => ID)
+  id!: string;
 
-//   @Column({ unique: true })
-//   @Field()
-//   email!: string;
+  @Column({ unique: true })
+  @Field()
+  email!: string;
 
-//   @Column()
-//   @Field()
-//   firstName!: string;
+  @Column()
+  @Field()
+  firstName!: string;
 
-//   @Column()
-//   @Field()
-//   lastName!: string;
+  @Column()
+  @Field()
+  lastName!: string;
 
-//   @OneToMany(() => Ride, (ride) => ride.user)
-//   rides!: Ride[];
+  @Column()
+  hashedPassword!: string;
 
-//   constructor(user?: CreateOrUpdateUser) {
-//     super();
+  @OneToMany(() => Ride, (ride) => ride.owner)
+  rides!: Ride[];
 
-//     if (user) {
-//       this.email = user.email;
-//       this.firstName = user.firstName;
-//       this.lastName = user.lastName;
-//     }
-//   }
-// }
+  constructor(user?: CreateOrUpdateUser) {
+    super();
 
-// export default User;
+    if (user) {
+      this.email = user.email;
+      this.firstName = user.firstName;
+      this.lastName = user.lastName;
+      this.hashedPassword = user.password;
+    }
+  }
+
+  static async createUser(userData: CreateOrUpdateUser): Promise<User> {
+    userData.password = await hash(userData.password, 10);
+
+    const newUser = new User(userData);
+    // TODO: return user-friendly error message when email already used
+    const savedUser = await newUser.save();
+    return savedUser;
+  }
+
+  static async getUserWithEmailAndPassword({
+    email,
+    password,
+  }: SignInUser): Promise<User> {
+    const user = await User.findOne({ where: { email } });
+    if (!user || !(await compare(password, user.hashedPassword))) {
+      throw new Error("INVALID_CREDENTIALS");
+    }
+    return user;
+  }
+
+  static async signIn({
+    email,
+    password,
+  }: SignInUser): Promise<{ user: User }> {
+    const user = await this.getUserWithEmailAndPassword({ email, password });
+    return { user };
+  }
+}
+
+export default User;
