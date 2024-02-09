6a0621834a614e16a63746edaa7d10a5e909eddd
SimNed <simon.nedjari@outlook.com>
2024-02-09 10:18:01 +0100

    update/ form and input components props

--- web-app/src/components/FormElements/Form/Form.tsx
+++ web-app/src/components/FormElements/Form/Form.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react"
 import { FormStyled } from "./Form.styled"
 
-const Form = ({ children }: { children: ReactNode }) => {
+const Form = ({ children, onSubmit }: { children :ReactNode; onSubmit: (event: React.FormEvent<HTMLInputElement>) => void }) => {
     return(
-        <FormStyled>{children}</FormStyled>
+        <FormStyled onSubmit={onSubmit}>{children}</FormStyled>
     )
 }
 
--- web-app/src/components/FormElements/Inputs/Input.tsx
+++ web-app/src/components/FormElements/Inputs/Input.tsx
@@ -1,5 +1,5 @@
 import { InputStyled } from './FormInputs';
 
-export const Input = ({ type, label }: { type: string; label: string }) => {
-  return <InputStyled type={type} placeholder={label} />;
+export const Input = ({ type, label, onChange }: { type: string; label: string; onChange: (event: React.ChangeEvent<HTMLInputElement>) => void }) => {
+  return <InputStyled type={type} placeholder={label} onChange={onChange}/>;
 };
