8a26457c20961bd97b7a1e09f288974c69f93248
SimNed <simon.nedjari@outlook.com>
2024-05-16 16:53:00 +0200

    change graphql codegen source !!

--- web-app/graphql-codegen.ts
+++ web-app/graphql-codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "http://localhost:4000",
-  documents: "src/**/*.tsx",
+  documents: "src/api-gql/**/*.ts",
   generates: {
     "src/gql/": {
       preset: "client",
