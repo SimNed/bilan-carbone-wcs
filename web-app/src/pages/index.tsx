b27d85f56667c720a4181296db978dd7a0bad552
SimNed <simon.nedjari@outlook.com>
2024-06-13 16:52:21 +0200

    change index page

--- web-app/src/pages/index.tsx
+++ web-app/src/pages/index.tsx
@@ -1,21 +1,65 @@
-import Layout from '@/components/Layout/Layout';
-import BaselineMUI from '@/pages/home_page/components/Baseline/BaselineMUI';
-import { Box, Grid } from '@mui/material';
+import { HEADER_HEIGHT, WHITE_COLOR } from "@/styles/constants";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function HomePage() {
   return (
-    <Layout>
-      <Grid
-        container
-        direction='column'
-        justifyContent='center'
-        alignItems='center'
-        style={{ minHeight: '80vh' }}
+    <>
+      <div
+        style={{
+          width: "100%",
+          background: WHITE_COLOR,
+          height: `calc(100vh - ${HEADER_HEIGHT})`,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
       >
-        <Box>
-          <BaselineMUI />
-        </Box>
-      </Grid>
-    </Layout>
+        <div style={{ textAlign: "center" }}>
+          <Typography variant="h1">Bilan Carbone</Typography>
+          <Typography
+            variant="h2"
+            sx={{
+              color: "#8d99ae",
+              fontFamily: "Poppins",
+              fontSize: "1.2rem",
+              fontWeight: "300",
+            }}
+          >
+            Calculer votre empreinte carbone et consulter les émissions de CO₂
+          </Typography>
+          <Box
+            m={6}
+            sx={{
+              display: "flex",
+              gap: "1rem",
+              justifyContent: "center",
+            }}
+          >
+            <Button
+              href="./carbonEmissionPage"
+              variant="contained"
+              size="large"
+              color="success"
+            >
+              Ajouter une dépense carbone
+            </Button>
+            <Button
+              href="./profil"
+              variant="outlined"
+              size="large"
+              color="success"
+            >
+              Voir mes dépenses carbone
+            </Button>
+          </Box>
+          <img
+            src="/images//world-snapshot.png"
+            alt=" world snapshot"
+            style={{ width: "40%" }}
+          />
+        </div>
+      </div>
+    </>
   );
 }
