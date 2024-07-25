a4a7f1825fb954a7ab8f773c0f96c2f4039585a9
SimNed <simon.nedjari@outlook.com>
2024-07-25 17:15:28 +0200

    update ride page logic

--- web-app/src/pages/rides/index.tsx
+++ web-app/src/pages/rides/index.tsx
@@ -1,9 +1,9 @@
 import { GetUserProfileQuery, SearchRidesQuery } from "@/gql/graphql";
 import { useMutation, useQuery } from "@apollo/client";
 import { SEARCH_RIDES } from "@/api-gql/queries/ride.queries";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { GET_USER_PROFIL } from "@/api-gql/queries/user.queries";
-import { Button, Container, Typography } from "@mui/material";
+import { Box, Button, Chip, Container, Grid, Typography } from "@mui/material";
 import { Stack } from "@mui/system";
 import RideCard from "./components/RideCard";
 import RideFilters from "./components/RideFilters";
@@ -11,6 +11,9 @@ import { useModal } from "@/components/layout/Layout";
 import { RideFilterData } from "@/type/RideFilterData.type";
 import DeleteRide from "./components/DeleteRide";
 import { DELETE_RIDE } from "@/api-gql/mutations/ride.mutations";
+import { DEFAULT_HEADER_HEIGHT, WHITE_COLOR } from "@/styles/constants";
+import LegendContainer from "@/components/containers/LegendContainer";
+import { STATISTICS_LEGEND_ELEMENTS } from "@/constants/charts.constants";
 
 const RidesPage = () => {
   const [filters, setFilters] = useState<RideFilterData>({});
@@ -44,7 +47,6 @@ const RidesPage = () => {
   };
 
   const handleDeleteRideConfirmation = (rideId: number) => {
-    console.log("RIDE ID", rideId);
     deleteRideMutation({
       variables: { id: rideId },
       refetchQueries: [{ query: SEARCH_RIDES, variables: { ...filters } }],
@@ -57,54 +59,139 @@ const RidesPage = () => {
     handleCloseModal();
   };
 
-  useEffect(() => {
+  const handleDeleteFilter = (key: keyof RideFilterData) => {
+    const newFilters = { ...filters };
+    delete newFilters[key];
+    setFilters(newFilters);
     refetch();
-  }, [refetch, filters]);
+  };
+
+  useEffect(() => {
+    console.log("Filters", filters);
+  }, [filters]);
 
   if (loading || typeof data === "undefined") return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  return data.searchRides.length > 0 ? (
-    <Stack direction="row">
-      <Stack direction="column" spacing={1}>
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() =>
-            handleModalComponent(
-              <RideFilters
-                handleRideFilter={handleRideFilter}
-                closeModal={handleCloseModal}
-              />
-            )
-          }
+  return (
+    <Stack
+      direction="column"
+      width="100%"
+      height="100%"
+      sx={{ backgroundColor: WHITE_COLOR }}
+    >
+      <Grid
+        container
+        direction="row"
+        justifyContent="space-between"
+        alignItems="center"
+        height={DEFAULT_HEADER_HEIGHT}
+        sx={{ backgroundColor: WHITE_COLOR }}
+        width="100%"
+        position="sticky"
+        top={DEFAULT_HEADER_HEIGHT}
+        zIndex={100}
+      >
+        <Grid
+          container
+          item
+          xs={12}
+          md={6}
+          direction="row"
+          justifyContent={{ xs: "space-between", md: "flex-start" }}
+          alignItems="center"
+          sx={{ backgroundColor: WHITE_COLOR }}
         >
-          Filtrer les trajets
-        </Button>
-        {data &&
-          data.searchRides.map((ride) => (
-            <RideCard
-              key={ride.id}
-              ride={ride}
-              handleDeleteRide={handleDeleteRide}
+          <Typography variant="h5" mx={4}>
+            {`${userData?.getUserProfile.firstName} ${userData?.getUserProfile.lastName}`}
+          </Typography>
+
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{ display: { xs: "block", md: "none" } }}
+            onClick={() =>
+              handleModalComponent(
+                <RideFilters
+                  handleRideFilter={handleRideFilter}
+                  closeModal={handleCloseModal}
+                />
+              )
+            }
+          >
+            Filtrer les trajets
+          </Button>
+        </Grid>
+        <Grid item xs={12} md={6} height={DEFAULT_HEADER_HEIGHT}>
+          {Object.keys(filters).map((key) => (
+            <Chip
+              key={key}
+              label={`${key}: ${filters[key as keyof RideFilterData]}`}
+              onDelete={() => handleDeleteFilter(key as keyof RideFilterData)}
             />
           ))}
-      </Stack>
+        </Grid>
+      </Grid>
+
+      <Grid
+        container
+        position="relative"
+        direction="row"
+        top={{
+          xs: `calc(${DEFAULT_HEADER_HEIGHT})`,
+          md: 0,
+        }}
+        height={`calc(100vh - ${DEFAULT_HEADER_HEIGHT} * 2)`}
+      >
+        <Grid
+          container
+          item
+          display={{ xs: "none", md: "grid" }}
+          xs={4}
+          height={`calc(100vh - ${DEFAULT_HEADER_HEIGHT} * 2)`}
+          justifyContent="center"
+        >
+          <Grid
+            container
+            item
+            md={4}
+            position="fixed"
+            top={`${DEFAULT_HEADER_HEIGHT} * 2`}
+            justifyContent="center"
+          >
+            <RideFilters
+              handleRideFilter={handleRideFilter}
+              closeModal={handleCloseModal}
+            />
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          p={8}
+          height={`calc(100vh - ${DEFAULT_HEADER_HEIGHT} * 2)`}
+        >
+          {data && data.searchRides.length > 0 ? (
+            data.searchRides.map((ride) => (
+              <RideCard
+                key={ride.id}
+                ride={ride}
+                handleDeleteRide={handleDeleteRide}
+              />
+            ))
+          ) : (
+            <Stack
+              justifyContent="center"
+              alignItems="center"
+              height={`calc(100vh - ${DEFAULT_HEADER_HEIGHT} * 2)`}
+            >
+              <Typography paragraph>Aucun trajet.</Typography>
+            </Stack>
+          )}
+        </Grid>
+      </Grid>
     </Stack>
-  ) : (
-    <Container
-      component="main"
-      maxWidth="xs"
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        margin: 0,
-        p: 6,
-      }}
-    >
-      <Typography paragraph>Aucun trajet enregistré.</Typography>
-    </Container>
   );
 };
 
