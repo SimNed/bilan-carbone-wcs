9221cb8e8e3a9a0cf96e3fe73570d5023dbcd391
SimNed <simon.nedjari@outlook.com>
2024-07-30 16:02:34 +0200

    add link to mui theme

--- web-app/src/styles/mui-theme.ts
+++ web-app/src/styles/mui-theme.ts
@@ -92,6 +92,28 @@ theme = createTheme(theme, {
         },
       },
     },
+    MuiLink: {
+      styleOverrides: {
+        root: {
+          margin: 0,
+          padding: 0,
+          height: "100%",
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontFamily: TITLE_FONT_FAMILY,
+          fontSize: ".8rem",
+          fontWeight: 800,
+          color: "inherit",
+          textDecoration: "none",
+          transition: "ease .3s",
+          "&:hover": {
+            backgroundColor: "rgba(0,0,0,.05)",
+          },
+        },
+      },
+    },
     MuiTypography: {
       styleOverrides: {
         root: {
