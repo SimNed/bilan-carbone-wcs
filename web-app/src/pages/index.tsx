d4205583e09c1f12d0cbf625db83e6c50a60c6e9
SimNed <simon.nedjari@outlook.com>
2024-07-30 17:57:29 +0200

    change index

--- web-app/src/pages/index.tsx
+++ web-app/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { useAuth } from "@/AuthProvider";
 import SignInForm from "@/components/auth/SignInForm";
 import { useModal } from "@/components/layout/Layout";
-import { WHITE_COLOR } from "@/styles/constants";
-import { Box, Button, Typography } from "@mui/material";
+import { DARK_GRAY_COLOR, WHITE_COLOR } from "@/styles/constants";
+import { Box, Button, Grid, Stack, Typography } from "@mui/material";
 import { useRouter } from "next/router";
 
 export default function HomePage() {
@@ -10,40 +10,31 @@ export default function HomePage() {
   const { handleModalComponent } = useModal();
   const router = useRouter();
   return (
-    <div
-      style={{
-        width: "100%",
-        background: WHITE_COLOR,
-        height: "100%",
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div style={{ textAlign: "center" }}>
-        <Typography variant="h1">Bilan Carbone</Typography>
-        <Typography variant="h5" color="#8d99ae">
-          Calculer votre empreinte carbone et consulter les émissions de CO₂
-        </Typography>
-        <Box
-          m={6}
-          sx={{
-            display: "flex",
-            gap: "1rem",
-            justifyContent: "center",
-          }}
-        >
+    <Stack flexGrow={1} justifyContent="center" alignItems="center" gap={4}>
+      <Typography variant="h1">Bilan Carbone</Typography>
+      <Typography variant="h5" color={DARK_GRAY_COLOR} align="center" px={2}>
+        Calculer votre empreinte carbone et consulter les émissions de CO₂
+      </Typography>
+      <Grid
+        container
+        my={2}
+        direction={{ xs: "column", md: "row" }}
+        justifyContent="center"
+        alignItems="center"
+        gap={2}
+      >
+        <Grid item xs={8} md={4} lg={3}>
           <Button
             variant="contained"
+            fullWidth
             size="large"
             color="success"
             onClick={() =>
               user
-                ? router.push("./carbonEmissionPage")
+                ? router.push("./add-ride")
                 : handleModalComponent(
                     <SignInForm
-                      onValidationRedirectionPath="./carbonEmissionPage"
+                      onValidationRedirectionPath="./add-ride"
                       subtitle="Pour ajouter une empreinte carbone vous devez d'abord vous connecter"
                     />
                   )
@@ -51,16 +42,20 @@ export default function HomePage() {
           >
             Ajouter une dépense carbone
           </Button>
+        </Grid>
+        <Grid item xs={8} md={4} lg={3}>
           <Button
+            sx={{ m: 0 }}
             variant="outlined"
+            fullWidth
             size="large"
             color="success"
             onClick={() =>
               user
-                ? router.push("./profil")
+                ? router.push("./rides")
                 : handleModalComponent(
                     <SignInForm
-                      onValidationRedirectionPath="./carbonEmissionPage"
+                      onValidationRedirectionPath="./rides"
                       subtitle="Pour votre votre empreinte carbone vous devez d'abord vous connecter"
                     />
                   )
@@ -68,13 +63,13 @@ export default function HomePage() {
           >
             Voir mes dépenses carbone
           </Button>
-        </Box>
-        <img
-          src="/images//world-snapshot.png"
-          alt=" world snapshot"
-          style={{ width: "40%" }}
-        />
-      </div>
-    </div>
+        </Grid>
+      </Grid>
+      <img
+        src="/images//world-snapshot.png"
+        alt=" world snapshot"
+        style={{ width: "40%" }}
+      />
+    </Stack>
   );
 }
