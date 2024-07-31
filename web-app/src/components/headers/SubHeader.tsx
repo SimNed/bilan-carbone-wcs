1ee00b09c4836e9a4b7f477b5e33eb4eac6b4876
SimNed <simon.nedjari@outlook.com>
2024-07-31 14:00:21 +0200

    create SubHeader component

---
+++ web-app/src/components/headers/SubHeader.tsx
@@ -0,0 +1,37 @@
+import { DEFAULT_HEADER_HEIGHT, WHITE_COLOR } from "@/styles/constants";
+import { Grid } from "@mui/material";
+import { ReactNode } from "react";
+
+const SubHeader = ({
+  leftChildren,
+  rightChildren,
+}: {
+  leftChildren: ReactNode;
+  rightChildren: ReactNode;
+}) => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent={{ xs: "space-between", md: "flex-start" }}
+      alignItems="center"
+      position="sticky"
+      top={DEFAULT_HEADER_HEIGHT}
+      width="100%"
+      height={DEFAULT_HEADER_HEIGHT}
+      zIndex={100}
+      px={4}
+      sx={{ backgroundColor: WHITE_COLOR }}
+    >
+      <Grid item xs={12} md={6} direction="row" alignItems="center">
+        {leftChildren}
+      </Grid>
+
+      <Grid item xs={12} md={6} direction="row" alignItems="center">
+        {rightChildren}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default SubHeader;
