06c7a52585c02d87543704ca3b2fe6212c22efc3
Simon Nedjari <simon.nedjari@outlook.com>
2024-08-03 20:21:53 +0200

    change 404 stack direction

--- web-app/src/pages/404.tsx
+++ web-app/src/pages/404.tsx
@@ -2,7 +2,7 @@ import { Stack, Typography } from "@mui/material";
 
 const Page404 = () => {
   return (
-    <Stack direction="row" justifyContent="center" alignItems="center">
+    <Stack direction="column" justifyContent="center" alignItems="center">
       <Typography variant="h1">404</Typography>
       <Typography paragraph>Page not found...</Typography>
     </Stack>
